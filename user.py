f3b2632e7ff87008159004df58c7608432182feb
Henriquebap <henriquebaptista2003@gmail.com>
2023-05-16 21:48:02 -0300

    Colocando execoes no codigo, mas existe um erro de validacao e criacao de user

--- user.py
+++ user.py
@@ -1,5 +1,8 @@
-users = []
 import re
+from passlib.hash import bcrypt
+
+users = []
+
 def login(users):
     id_mail = input("Digite o seu email: ")
     id_password = input("Digite a senha: ")
@@ -20,13 +23,52 @@ def remove_user(users):
 
 def cadastro_user():
     first_name = input("Digite seu primeiro nome: ")
-    first_name = re.sub(r'[^a-z]','',first_name)#improve this
+    try:
+        first_name = re.sub(r'[^a-z]','',first_name)#improve this
+        first_name = str(first_name)
+    except:
+        print("Porfavor, digite o seu nome somente com Letras")
     last_name = input("Digite seu sobrenome: ")
-    last_name = re.sub(r'[^a-z]','',last_name)
+    try:
+        last_name = re.sub(r'[^a-z]','',last_name)
+    except:
+        print("Porfavor Digite o seu sobrenome somente com letras")
     CPF = input("Digite o seu CPF: ")#number treatment too
-    email = input("Digite o seu email: ")
+    try:
+        CPF.isalpha
+        CPF = re.sub('[^0-9]', '', CPF)
+        if len(CPF) != 11:
+            return "CPF inválido. Verifique a quantidade de dígitos."
+    except:
+        print("Porfavor Coloque somente numeros no seu cpf")
+
+    cpf_formatado = f"{CPF[:3]}.{CPF[3:6]}.{CPF[6:9]}-{CPF[9:]}"
+
+    email = input("Digite o seu email: ").strip()
+    try:
+        padrao = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        if not re.match(padrao, email):
+            return "Porfavor verifique o formato do seu email"
+    except:
+        print("Dgiite um email valido")
     #we want make string treatment here. Just to remember
+    def hash_id(identification):
+        hashed_id = bcrypt.hash(identification)
+        return hashed_id
+    def verify_id(identification, hashed_id):
+        return bcrypt.verify(identification, hashed_id)
+    
     identification = input("Crie uma Senha: ")
+
+    hashed_id = hash_id(identification)
+    
+    id_input = input("Digite a sua senha novamente para verificar: ")
+    id_match = verify_id(id_input, hashed_id)
+    if id_match:
+        print("Senha Salva com sucesso")
+    else:
+        print("Senha Incorreta")
+
     user = {
         'first_name': first_name,
         'last_name': last_name,
