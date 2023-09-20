ca3aa675719a08f898f302e0e2233f471043fa89
Henriquebap <henriquebaptista2003@gmail.com>
2023-09-19 21:42:39 -0300

    Arrumando algumas coisas da API para a entrega- TODO - Banco de Dados, melhor implementacao da API (Salvar imagens, INPUT para USER e OUTPUT) Funcoes mais enxugadas

--- api_ia.py
+++ api_ia.py
@@ -1,8 +1,6 @@
 from roboflow import Roboflow
 import os
-#import json
 
-#print("Current working directory:", os.getcwd())
 def cv_api():
     # Initialize the Roboflow object with your API key
     rf = Roboflow(api_key="fFzjCpFlDM2cN1D0XgOj")
@@ -12,28 +10,40 @@ def cv_api():
     model = project.version(2).model
 
     # Get the image path from user input
-    image_path = input("Enter the path to the image: ")
+    image_path = input("Coloque o caminho da imagem: ")
 
     # Check if the image file exists
     if not os.path.isfile(image_path):
-        print("Image file not found.")
+            print("Image file not found.")
     else:
         # Make predictions
-        prediction_result = model.predict(image_path, confidence=40, overlap=30).json()
+        prediction_result = model.predict(image_path, confidence=50, overlap=50).json()
 
-        # Define a function to explore the JSON response
-    def explore_json(obj, prefix=""):
-        if isinstance(obj, dict):
-            for key, value in obj.items():
-                explore_json(value, prefix + key + ".")
-        elif isinstance(obj, list):
-            for index, item in enumerate(obj):
-                explore_json(item, prefix + str(index) + ".")
-        else:
-            print(f"Campo: {prefix[:-1]}, Valor: {obj}")
+            # Explore the JSON response
+        try:
+            # Check if 'predictions' key exists in the JSON response
+            if 'predictions' in prediction_result:
+                predictions = prediction_result['predictions']
 
-        # Explore the JSON response
-    explore_json(prediction_result)
+                # Loop through predictions
+                for index, prediction in enumerate(predictions):
+                    print(f"Prediction {index + 1}:")
+                    confidence = prediction.get('confidence', None)
+                    predicted_class = prediction.get('class', None)
+
+                    if confidence is not None and predicted_class is not None:
+                        if confidence > 0.50 or predicted_class != 'bike':
+                            print("a foto da bicicleta foi aprovada!")
+                        else:
+                            print("Infelizmente a foto da sua bicicleta foi recusada.")
+                    else:
+                        print("Error: Incomplete prediction data.")
+            else:
+                print("Error: 'predictions' key not found in the JSON response.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    
 
     # Specify the folder where you want to save the prediction image
     output_folder = "received_images"
@@ -44,5 +54,5 @@ def cv_api():
         
     # Save the prediction image
     prediction_image_path = os.path.join(output_folder, "prediction.jpg")
-    model.predict(image_path, confidence=40, overlap=30).save(prediction_image_path)
+    model.predict(image_path, confidence=50, overlap=50).save(prediction_image_path)
     print(f"Prediction image saved as {prediction_image_path}")
